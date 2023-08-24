73ec25737d58b44a1c9d402bc4a5cc221eadb433
malikafrontend2023 <malikanasimova03@gmail.com>
2023-08-24 19:35:33 +0500

    Add search link butoon click  handler. wrap form submit handler in if statemant

--- js/main.js
+++ js/main.js
@@ -1,8 +1,29 @@
+const elSearchLink = document.querySelector('.search-link');
 const elSearchForm = document.querySelector('.js-search-form');
 const elSearchLinkResults = document.querySelector('.search-link__results');
 
-elSearchForm.addEventListener('submit', function (evt) {
-  evt.preventDefault();
+if (elSearchLink) {
+  elSearchForm.addEventListener('submit', function (evt) {
+    evt.preventDefault();
 
-  elSearchLinkResults.classList.add('search-link__results--open');
-})
+    elSearchLinkResults.classList.add('search-link__results--open');
+  });
+}
+
+if (elSearchLink) {
+  elSearchLink.addEventListener('click', function (evt) {
+    if (evt.target.matches('.js-copy-search-link-button')) {
+      // Change button text
+      evt.target.textContent = 'Copied!';
+
+      // Change button bgcolor
+      evt.target.classList.add('search-link__copy-button--copied');
+    }
+
+    // Reset button text and class after 1 second
+    setTimeout( function () {
+      evt.target.textContent = 'Copy';
+      evt.target.classList.remove('search-link__copy-button--copied');
+    }, 1000);
+  });
+}
